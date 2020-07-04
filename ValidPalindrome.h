ced4bfe94e66130147bc98ea08f5e3082c185f27
MAYANK-BHARDWAJ <44526282+MAYANK-BHARDWAJ@users.noreply.github.com>
2020-07-04 09:41:05 +0530

    Update ValidPalindrome.h
    (1)   No need to create extra variable "j".
    (2)   We can define string length outside the for loop, by doing so the for loop will not compute the string size in every iteration, hence it reduces the computation time.
    (3)   No need to traverse the whole string we can achieve our goal by just traversing till the midway, this reduces the time complexity from O(n) to O(n/2), where n is length of the string

--- ValidPalindrome.h
+++ ValidPalindrome.h
@@ -21,13 +21,11 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        for (int i = 0, j = s.size() - 1; i < j; ++i, --j)
+        int stringLength = l;
+        for (int i = 0, i < l/2; ++i)
         {
-            while (i < j && !isalnum(s[i])) i++;
-            while (i < j && !isalnum(s[j])) j--;
-
-            if (tolower(s[i]) != tolower(s[j]))
-                return false;
+             if (tolower(s[i]) != tolower(s[l-i-1]))
+                 return false;
         }
         return true;
     }
